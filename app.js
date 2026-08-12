d914c246a6612f6fbd2ab0188daf33318e55c199
charleytrigano <charley@trigano.org>
2026-08-12 18:06:39 +0200

    Update Supabase key in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // CONFIGURATION SUPABASE OFFICIELLE
 // ==========================================
 const SUPABASE_URL = "https://qfwhzuhwldurnmhirgil.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmd2h6dWh3bGR1cm5tagirgilIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0OTQ0MTgsImV4cCI6MjEwMTA3MDQxOH0.Lt7eU9UBVY94tIIMUNOzLeJOpWnkGkvszy_gENkUkLg";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmd2h6dWh3bGR1cm5taGlyZ2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0OTQ0MTgsImV4cCI6MjEwMTA3MDQxOH0.Lt7eU9UBVY94tIIMUNOzLeJOpWnkGkvszy_gENkUkLg";
 
 // Initialisation du client Supabase
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
